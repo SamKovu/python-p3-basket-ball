0b43823f714ef3ccfbb0fb947acc42459478729c
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-12-05 05:48:26 +0300

    Completed lab

---
+++ lib/basket_ball.py
@@ -0,0 +1,298 @@
+def game_dict():
+    return {
+        "home": {
+            "team_name": "Cleveland Cavaliers",
+            "colors": ["Wine", "Gold"],
+            "players": [
+                {
+                    "name": "Jarrett Allen",
+                    "number": 31,
+                    "position": "Center",
+                    "points_per_game": 16.1,
+                    "rebounds_per_game": 10.8,
+                    "assists_per_game": 1.6,
+                    "steals_per_game": 0.8,
+                    "blocks_per_game": 1.3,
+                    "career_points": 3945,
+                    "age": 24,
+                    "height_inches": 82,
+                    "shoe_brand": "Nike",
+                },
+                {
+                    "name": "Darius Garland",
+                    "number": 10,
+                    "position": "Point Guard",
+                    "points_per_game": 21.7,
+                    "rebounds_per_game": 3.3,
+                    "assists_per_game": 8.6,
+                    "steals_per_game": 1.3,
+                    "blocks_per_game": 0.1,
+                    "career_points": 3142,
+                    "age": 22,
+                    "height_inches": 73,
+                    "shoe_brand": "Nike",
+                },
+                {
+                    "name": "Evan Mobley",
+                    "number": 4,
+                    "position": "Center",
+                    "points_per_game": 15.0,
+                    "rebounds_per_game": 8.3,
+                    "assists_per_game": 2.5,
+                    "steals_per_game": 0.8,
+                    "blocks_per_game": 1.7,
+                    "career_points": 1034,
+                    "age": 21,
+                    "height_inches": 83,
+                    "shoe_brand": "Adidas",
+                },
+                {
+                    "name": "Kevin Love",
+                    "number": 0,
+                    "position": "Power Forward",
+                    "points_per_game": 13.6,
+                    "rebounds_per_game": 7.2,
+                    "assists_per_game": 2.2,
+                    "steals_per_game": 0.4,
+                    "blocks_per_game": 0.2,
+                    "career_points": 14305,
+                    "age": 34,
+                    "height_inches": 80,
+                    "shoe_brand": "Nike",
+                },
+                {
+                    "name": "Isaac Okoro",
+                    "number": 35,
+                    "position": "Small Forward",
+                    "points_per_game": 8.8,
+                    "rebounds_per_game": 3.0,
+                    "assists_per_game": 1.8,
+                    "steals_per_game": 0.8,
+                    "blocks_per_game": 0.3,
+                    "career_points": 1234,
+                    "age": 21,
+                    "height_inches": 77,
+                    "shoe_brand": "Nike",
+                },
+                {
+                    "name": "Ricky Rubio",
+                    "number": 99,
+                    "position": "Point Guard",
+                    "points_per_game": 13.1,
+                    "rebounds_per_game": 4.1,
+                    "assists_per_game": 6.6,
+                    "steals_per_game": 1.4,
+                    "blocks_per_game": 0.2,
+                    "career_points": 7399,
+                    "age": 31,
+                    "height_inches": 74,
+                    "shoe_brand": "Adidas",
+                },
+            ],
+        },
+            
+        "away": {
+            "team_name": "Washington Wizards",
+            "colors": ["Red", "White", "Navy Blue"],
+            "players": [   
+                {
+                    "name": "Bradley Beal",
+                    "number": 3,
+                    "position": "Shooting Guard",
+                    "points_per_game": 23.2,
+                    "rebounds_per_game": 4.7,
+                    "assists_per_game": 6.6,
+                    "steals_per_game": 0.9,
+                    "blocks_per_game": 0.4,
+                    "career_points": 14231,
+                    "age": 29,
+                    "height_inches": 76,
+                    "shoe_brand": "Nike",
+                },
+                {
+                    "name": "Kyle Kuzma",
+                    "number": 33,
+                    "position": "Power Forward",
+                    "points_per_game": 17.1,
+                    "rebounds_per_game": 8.5,
+                    "assists_per_game": 3.5,
+                    "steals_per_game": 0.6,
+                    "blocks_per_game": 0.9,
+                    "career_points": 5336,
+                    "age": 27,
+                    "height_inches": 81,
+                    "shoe_brand": "Puma",
+                },
+                {
+                    "name": "Kentavious Caldwell-Pope",
+                    "number": 1,
+                    "position": "Shooting Guard",
+                    "points_per_game": 13.2,
+                    "rebounds_per_game": 3.4,
+                    "assists_per_game": 1.9,
+                    "steals_per_game": 1.1,
+                    "blocks_per_game": 0.3,
+                    "career_points": 7911,
+                    "age": 29,
+                    "height_inches": 77,
+                    "shoe_brand": "Nike",
+                },
+                {
+                    "name": "Davis Bertans",
+                    "number": 42,
+                    "position": "Power Forward",
+                    "points_per_game": 5.6,
+                    "rebounds_per_game": 2.1,
+                    "assists_per_game": 0.6,
+                    "steals_per_game": 0.3,
+                    "blocks_per_game": 0.3,
+                    "career_points": 3165,
+                    "age": 29,
+                    "height_inches": 82,
+                    "shoe_brand": "Nike",
+                },
+                {
+                    "name": "Kristaps Porzingis",
+                    "number": 6,
+                    "position": "Power Forward",
+                    "points_per_game": 22.1,
+                    "rebounds_per_game": 8.8,
+                    "assists_per_game": 2.9,
+                    "steals_per_game": 0.7,
+                    "blocks_per_game": 1.5,
+                    "career_points": 6371,
+                    "age": 27,
+                    "height_inches": 87,
+                    "shoe_brand": "Adidas",
+                },
+                {
+                    "name": "Rui Hachimura",
+                    "number": 8,
+                    "position": "Power Forward",
+                    "points_per_game": 11.3,
+                    "rebounds_per_game": 3.8,
+                    "assists_per_game": 1.1,
+                    "steals_per_game": 0.5,
+                    "blocks_per_game": 0.2,
+                    "career_points": 1913,
+                    "age": 24,
+                    "height_inches": 80,
+                    "shoe_brand": "Jordan",
+                },
+            ]
+        }
+    }
+
+
+
+def get_all_players():
+    return game_dict()["home"]["players"]+game_dict()["away"]["players"]
+
+def num_points_per_game(name):
+    for player in get_all_players():
+        if player["name"]==name:
+            return player["points_per_game"]
+        
+print(num_points_per_game("Rui Hachimura"))
+
+def player_age(name):
+    for player in get_all_players():
+        if player["name"]==name:
+            return player["age"]
+
+print(player_age("Rui Hachimura"))
+
+def team_names():
+    return [v["team_name"] for k,v in game_dict().items()]
+   
+
+print(team_names())
+
+def home_or_away(name):
+    if game_dict()["home"]["team_name"]==name:
+        return "home"
+    if game_dict()["away"]["team_name"]==name:
+        return "away"
+
+def team_colors(name):
+    team=home_or_away(name)
+    if game_dict()[team]["team_name"]==name:
+        return game_dict()[team]["colors"]
+
+
+print(team_colors("Cleveland Cavaliers"))
+
+def player_numbers(name):
+     team=home_or_away(name)
+     if game_dict()[team]["team_name"]==name:
+        return [x["number"] for x in game_dict()[team]["players"] ]
+   
+     
+print(player_numbers("Cleveland Cavaliers"))
+
+def player_stats(name):
+     for player in get_all_players():
+        if player["name"]==name:
+            return player
+
+print(player_stats("Rui Hachimura"))
+
+print("\n\n")
+
+
+def shoe_brands():
+     brands=[]
+     for player in get_all_players():
+        if player["shoe_brand"] not in brands:
+            brands.append(player["shoe_brand"])
+     return brands
+
+print(shoe_brands())
+
+def shoe_brand_dic():
+    brands={}
+    for brand in shoe_brands():
+        brands.update({brand:[]})
+    
+    for brand in shoe_brands():
+        for player in get_all_players():
+            if player["shoe_brand"]==brand:
+                brands[brand].append(player["rebounds_per_game"])
+
+    return brands
+
+print (shoe_brand_dic())
+
+print("\n\n")
+
+def mean(numbers):
+    total=0
+    for n in numbers:
+        total=total+n
+    return total/len(numbers)
+
+def average_rebounds_by_shoe_brand():
+    for key,value in shoe_brand_dic().items():
+        x = '{0:.2f}'.format(mean(value))
+        print(f"{key}:  {x}")
+
+average_rebounds_by_shoe_brand()
+
+
+
+
+
+
+
+
+
+    
+        
+
+    
+
+
+
+
+
+    
